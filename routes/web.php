002d0004634f56159ef8de6de4d1f547f1c6ab28
anderDaZu <anderda-96@hotmail.com>
2024-08-20 19:40:02 -0500

    Recursividad

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,32 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('prueba', function () {
+
+    $array1 = ['a', 'b', 'c'];
+    $array2 = ['a', 'b', 'c'];
+    $array3 = ['a', 'b', 'c'];
+
+    $arrays = [$array1, $array2, $array3];
+
+    $combinaciones = generarCombinaciones($arrays);
+    return $combinaciones;
+});
+
+function generarCombinaciones( $arrays, $indice = 0, $combinacion = [] )
+{
+    if ( $indice == count($arrays) ) {
+        return [$combinacion];
+    }
+
+    $resultado = [];
+
+    foreach ( $arrays[$indice] as $item ) {
+        $combinacionTemporal = $combinacion;
+        $combinacionTemporal[] = $item;
+        $resultado = array_merge($resultado, generarCombinaciones($arrays, $indice + 1, $combinacionTemporal));
+    }
+
+    return $resultado;
+}
